0f2f9ae0ff249c9cf8b3ec56a61b5b48a6f57083
Justin Latta <null7238@gmail.com>
2023-11-29 00:33:10 -0600

    google reviews mobile and slide

--- google-reviews.js
+++ google-reviews.js
@@ -1,9 +1,50 @@
-/*$(".gr-show-more").on('click', function(event) {
-            $(this).find(".gr-text").addClass("gr-text-expanded");
-        });*/
-
-
 document.addEventListener('DOMContentLoaded', function(){
+    let flexWrapper = document.querySelector('.gr-flex-wrapper');
+    let allReviews = document.querySelectorAll('.gr-review');
+    let lastReview = allReviews[allReviews.length - 1];
+    
+    let startX = 0;
+    let currentX = 0;
+    let mouseDown = false;
+    let lastX = 0;
+
+    function sliderMove(e) {
+        if(mouseDown) {
+            let x = e.x ?? e.touches[0].clientX;
+            currentX = x - startX + lastX;
+            flexWrapper.style.transform = "translate3d(" + currentX + "px, 0px, 0px)";
+        }
+    }
+
+    function sliderStart(e) {
+        let x = e.x ?? e.touches[0].clientX;
+        startX = x;
+        mouseDown = true;
+    }
+
+    function sliderEnd(e) {
+        mouseDown = false;
+
+        if(currentX > 0) {
+            currentX = 0;
+        }
+
+        if(lastReview.getBoundingClientRect().right < flexWrapper.offsetWidth) {
+            currentX = (flexWrapper.scrollWidth - flexWrapper.offsetWidth) * -1;
+        }
+
+        flexWrapper.style.transform = "translate3d(" + currentX + "px, 0px, 0px)";
+
+        lastX = currentX;  
+    }
+
+    flexWrapper.addEventListener("mousemove",sliderMove);
+    flexWrapper.addEventListener("mousedown",sliderStart);
+    flexWrapper.addEventListener("mouseup",sliderEnd);
+
+    flexWrapper.addEventListener("touchmove",sliderMove);
+    flexWrapper.addEventListener("touchstart",sliderStart);
+    flexWrapper.addEventListener("touchend",sliderEnd);
 
     document.querySelectorAll('.gr-text').forEach((div => {
         if(div.offsetHeight >= 85) {
